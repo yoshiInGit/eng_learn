1fef7e2af72e2a9def32f389722beef0585e8631
yoshiInGit <yoshimain016@gmail.com>
2025-06-14 15:59:45 +0900

    環境環境変数の名前を訂正

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -3,18 +3,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, // あなたのAPIキー
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, // あなたのSender ID
-  appId: process.env.FIREBASE_APP_ID, // あなたのApp ID
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID // Google Analyticsを使っている場合
+  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY, // あなたのAPIキー
+  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID, // あなたのSender ID
+  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID, // あなたのApp ID
+  measurementId:     process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID // Google Analyticsを使っている場合
 };
 
 // Firebaseを初期化
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-// Authenticationサービスを取得
+// // Authenticationサービスを取得
 export const auth = getAuth(app);
 
